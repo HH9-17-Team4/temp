4c35a6f869adf574da85e987cb5ae4c7578aee0d
sryung <sryung1225@gmail.com>
2023-10-13 00:12:00 +0900

    MBTI 테이블 속 데이터 테스트 결과 화면에 렌더링

--- app.py
+++ app.py
@@ -121,68 +121,22 @@ def result():
   # 다음 질문이 있으면 진행, 아니면 MBTI 계산
   if next_question:
     return render_template("test.html", data=next_question)
-  else:
-    calculated_mbti = calculate_mbti()
-    answer = MBTI.query.filter_by(mbti_type=calculated_mbti).first()
 
-  #calculated_mbti = calculate_mbti()
-  #description = c.execute('SELECT description FROM MBTI WHERE id = 1')
-  #book = MBTI.query.filter_by(mbti_type=calculated_mbti).get(book)
-  #image = MBTI.query.filter_by(mbti_type=calculated_mbti).get(image)
-  #for tr in trs:
-  #      rank = tr.select_one('.rank').text
-  #      title = tr.select_one('.rank01 > span > a').text
-  #      artist = tr.select_one('.rank02 > a').text
-  #      image = tr.select_one('img')['src']
-  #      melon_data.append({'rank': rank, 'artist': artist, 'title': title, 'image': image})
-  # 1. 테이블에 있는 쿼리를 스플릿 시킨다. => is not subscriptable
-  # 2. 쿼리에서 각각 데이터를 넣는다. =>
-  
-  def fetch_data_from_table(database_name, table_name):
-    try:
-      conn=sqlite3.connect(database_name)
+  calculated_mbti = calculate_mbti()
+  answer = MBTI.query.filter_by(mbti_type=calculated_mbti).first()
 
-      cursor =conn.cursor()
+  book_info = answer.book.split(" : ")
+  book_name = book_info[0]
+  book_desc = book_info[1]
 
-      sql_query=f"SELECT * FROM {table_name} WHERE mbti_type=calculated_mbti"
+  context = {
+    "mbti": calculated_mbti,
+    "desc": answer.description,
+    "book_name": book_name,
+    "book_desc": book_desc
+  }
 
-      cursor.execute(sql_query)
-
-      rows = cursor.fetchall
-
-      cursor.close
-
-      conn.close
-
-      return rows
-    
-    except sqlite3.Error as e:
-      print("SQLite 에러 :",e)
-      return None
-    
-  database_name = 'database.db'
-  table_name = 'my_table'
-  table_data =fetch_data_from_table(database_name,table_name)
-
-  if table_data:
-    for column in table_data:
-      answers = [column]
-  else :
-    print("데이터를 가져올 수 없었습니다")
-
-  description = "SELECT description FROM 'MBTI' WHERE mbti_type=calculated_mbti"
-  book = "SELECT book FROM 'MBTI' WHERE mbti_type=calculated_mbti"
-  image = "SELECT image FROM 'MBTI' WHERE mbti_type=calculated_mbti"
-
-  #answers=[]
-  #answers.append[{"type":calculated_mbti, "description":description,"book":book,"image":image}]
-  """
-  # 책들이 다수라면 book에 , 기준으로 책 제목을 나열하는 등의 방식 사용
-  # books = [], books = answer.book.split({"type":answer[0],"description":answer[1],"book":answer[2],"image":answer[3]})"""
-  # 저장된 책의 제목을 이용해서
-  # 설명이나 다른 정보를 크롤링 정보랑 연결하면 될 듯 해요!
-
-  return render_template("result.html", data=answers)
+  return render_template("result.html", data=context)
 
 @app.route("/map")
 def map():
